ff0826c92180195daf95f512222b22698f70cc29
Manoj yeduvaka <manojyeduvaka@gmail.com>
2026-05-28 17:35:27 +0530

    Implement median finding for two sorted arrays

---
+++ Median_of_two_sorted_arrays.py
@@ -0,0 +1,9 @@
+import statistics
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+
+        s = statistics.median(nums1)
+
+        return s 
